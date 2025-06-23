4137a422ebace79543d67eaa88f40cc1508101ef
Gilyana Bachaeva <gelchik29@icloud.com>
2025-06-23 20:39:56 +0300

    Initial commit

---
+++ src/main/java/com/example/LearningManageSystem/LearningManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearningManageSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningManageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LearningManageSystem/LearningManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearningManageSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningManageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
